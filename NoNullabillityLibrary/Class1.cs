aa6017c98ce0a70e6fc26dbcb59d8511d2a04ce0
denis.troller <denis.troller@sonarsource.com>
2023-10-05 10:58:23 +0200

    added other library

---
+++ NoNullabillityLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace NullabilityRulesCheckLibrary
+{
+    public class Class1
+    {
+        public void Method1(PassAround arg1)
+        {
+            arg1.Method1(Name);
+        }    
+
+        public string Name { get; set; }
+    }
+}
