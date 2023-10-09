be673fdd96dde6eeb70084ddc1317547ef240d0b
denis.troller <denis.troller@sonarsource.com>
2023-10-09 09:28:48 +0200

    Adde null check

--- NoNullabillityLibrary/Class1.cs
+++ NoNullabillityLibrary/Class1.cs
@@ -4,7 +4,12 @@
     {
         public void Method1(PassAround arg1)
         {
-            arg1.Method1(Name);
+            if(arg1 is null)
+            {
+                throw new ArgumentNullException(nameof(arg1));
+            }
+
+            arg1.Method1(Name); 
         }    
 
         public string Name { get; set; }
